3b6fd50cf31369bb2486643c4504a6ea22e4e3c3
Cryst4l <bhalimi@outlook.fr>
2021-12-10 14:04:50 +0100

    Force C98

--- Glover/Distance.h
+++ Glover/Distance.h
@@ -1,2 +1,10 @@
 #pragma once
 
+static int bowl(int x, int i, int g_i) {
+	return (x - i) * (x - i) + g_i * g_i;
+}
+static int sep(int i, int u, int g_i, int g_u)
+{
+	double r = floor((u * u - i * i + g_u * g_u - g_i * g_i) / (2 * (u - i)));
+	return (int) r;
+}
